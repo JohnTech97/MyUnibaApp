a3814e26dc444dafeabd0658e9b0f84d02786aee
drissouissiakavaleriofoule <foulevalerio@gmail.com>
2020-06-15 16:31:43 +0200

    LoginActivity.java implementata correttamente

--- app/src/main/java/sms/myunibapp/LoginActivity.java
+++ app/src/main/java/sms/myunibapp/LoginActivity.java
@@ -1,20 +1,38 @@
 package sms.myunibapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.myunibapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //Credenziali di accesso
     private EditText editTextUsername;
     private EditText editTextPassword;
+
+    //Operazioni per l'accesso
+    FirebaseAuth firebaseAuth;
+    Button mLoginButton;
+    ProgressBar progressBar;
+
+    //Modalità OSPITE
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -24,30 +42,59 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         editTextUsername = findViewById(R.id.username);
         editTextPassword = findViewById(R.id.password);
 
-        findViewById(R.id.button_login).setOnClickListener(this);
+        mLoginButton = findViewById(R.id.button_login);
+
+        firebaseAuth = FirebaseAuth.getInstance(); //collegamento a firebase
+
         findViewById(R.id.ospite).setOnClickListener(this);
         findViewById(R.id.lost_credenziali).setOnClickListener(this);
-    }
 
-    private void userLogin(){
+        //Evento che si verifica se clicco il tasto LOGIN
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        String username = editTextUsername.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
+                String username = editTextUsername.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
 
-        //Se il campo username è vuoto
-        if(username.isEmpty()){
-            editTextUsername.setError("Inserisci username");
-            editTextUsername.requestFocus();
-            return;
-        }
+                //Se il campo username è vuoto
+                if(username.isEmpty()){
+                    editTextUsername.setError("Inserisci username");
+                    editTextUsername.requestFocus();
+                    return;
+                }
 
+                //Se il campo password è vuoto
+                if(password.isEmpty()){
+                    editTextPassword.setError("Inserisci password");
+                    editTextPassword.requestFocus();
+                    return;
+                }
 
-        //Se il campo password è vuoto
-        if(password.isEmpty()){
-            editTextPassword.setError("Inserisci password");
-            editTextPassword.requestFocus();
-            return;
-        }
+                //Se la password è più corta di 6 caratteri
+                if(password.length() < 6){
+                    editTextPassword.setError("La password deve contenere almeno 6 caratteri");
+                    editTextPassword.requestFocus();
+                }
+
+                //Autenticazione dell'utente
+                username += "@studenti.uniba.it";
+                firebaseAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Login avvenuto con successo", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Home.class));
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 
     private void ospiteLogin(){
@@ -60,7 +107,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.button_login: userLogin(); break;
             case R.id.ospite: ospiteLogin(); break;
             case R.id.lost_credenziali: lostCredenziali(); break;
         }
