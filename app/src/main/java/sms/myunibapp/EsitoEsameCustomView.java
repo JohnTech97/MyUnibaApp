3335763da262023939933d325715f561ab0b1348
gionn <minetros4530@gmail.com>
2020-06-16 18:22:58 +0200

    Piccoli tweak grafici per il template esito esame, aggiunta delle schermate bacheca prenotazioni e bacheca esiti

--- app/src/main/java/sms/myunibapp/EsitoEsameCustomView.java
+++ app/src/main/java/sms/myunibapp/EsitoEsameCustomView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -11,10 +12,9 @@ import androidx.annotation.Nullable;
 
 public class EsitoEsameCustomView extends View {
 
-    private Paint positivo;
-    private Paint negativo;
+    private Paint positivoBorder, negativoBorder, positivo, negativo, fill;
 
-    private int voto=0;
+    private String esito = "0", bocciato = "Bocciato";
 
     public EsitoEsameCustomView(Context context) {
         super(context);
@@ -36,27 +36,61 @@ public class EsitoEsameCustomView extends View {
         initialize();
     }
 
-    private void initialize(){
-        positivo=new Paint();
+    private void initialize() {
+        positivoBorder = new Paint();
+        positivoBorder.setColor(Color.GREEN);
+        positivoBorder.setStyle(Paint.Style.STROKE);
+        positivoBorder.setStrokeWidth(10);
+        negativoBorder = new Paint();
+        negativoBorder.setColor(Color.RED);
+        negativoBorder.setStyle(Paint.Style.STROKE);
+        negativoBorder.setStrokeWidth(10);
+        positivo = new Paint();
+        positivo.setTextSize(50);
         positivo.setColor(Color.GREEN);
-        negativo=new Paint();
+        negativo = new Paint();
+        negativo.setTextSize(50);
         negativo.setColor(Color.RED);
+        fill = new Paint();
+        fill.setColor(Color.WHITE);
     }
 
-    public void setVoto(int voto){
-        this.voto=voto;
+    public void setEsito(String esito) {
+        this.esito = esito;
     }
 
-    public int getVoto(){
-        return voto;
+    public String getEsito() {
+        return esito;
+
     }
+
+    private int centroX, centroY;
+    private Rect bound = new Rect();
 
     @Override
     protected void onDraw(Canvas c) {
-        if(voto>=18) {
-            c.drawText(""+voto, 20, 20, positivo);
-        }else{
-            c.drawText("Bocciato", 20, 20, negativo);
+        centroX = c.getWidth() / 2;
+        centroY = c.getHeight() / 2;
+        //se l'esito dell'esame è un numero allora lo studente è promosso
+        try {
+            if (Integer.parseInt(esito) >= 18) {
+                //riempio prima uno spazio bianco
+                c.drawCircle(centroX, centroY, centroX, fill);
+                c.drawCircle(centroX, centroY, centroX - 10, positivoBorder);
+                negativo.getTextBounds(esito, 0, esito.length(), bound);
+                c.drawText(esito, centroX - bound.width() / 2, centroY + bound.height() / 2, positivo);
+            } else {
+                c.drawRect(10, 10, centroX * 2-10, centroY * 2-10, fill);
+                c.drawRect(10, 10, centroX * 2-10, centroY * 2-10, negativoBorder);
+                negativo.getTextBounds(bocciato, 0, bocciato.length(), bound);
+                c.drawText(bocciato, centroX - bound.width() / 2, centroY + bound.height() / 2, negativo);
+            }
+        } catch (NumberFormatException e) {
+            //altrimenti può avere un altro risvolto: assente, ritirato, ecc;
+            c.drawRect(10, 10, centroX * 2-10, centroY * 2-10, fill);
+            c.drawRect(10, 10, centroX * 2-10, centroY * 2-10, negativoBorder);
+            negativo.getTextBounds(esito, 0, esito.length(), bound);
+            c.drawText(esito, centroX - bound.width() / 2, centroY + bound.height() / 2, negativo);
         }
     }
 }
